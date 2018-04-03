658656153afd0783eed9d20810a376d28efb2353
Vicentecorrea <vicorrea@miuandes.cl>
2018-04-03 15:57:25 -0300

    fogdfogd

---
+++ Lab3VC/Lab3VC/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3VC
+{
+    class Cliente
+    {
+        string tipo;
+
+        public Cliente(string miTipo)
+        {
+            tipo = miTipo;
+        }
+        public void Arrendar()
+        {
+
+        }
+    }
+}
---
+++ Lab3VC/Lab3VC/Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3VC
+{
+    public class Vehiculo
+    {
+        string tipo;
+
+        public Vehiculo(string miTipo)
+        {
+            tipo = miTipo;
+        }
+
+    }
+    public class Auto
+    {
+
+    }
+}
